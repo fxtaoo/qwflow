598d125afc4c9fb7c78cef58c42904d82857d459
Fang Xuetao <fxtaoo@gmail.com>
2022-11-03 16:20:24 +0800

    直播 cdn 告警比例分开

--- alert/alert.go
+++ alert/alert.go
@@ -10,11 +10,12 @@ import (
 
 // 一天警告警告
 type Alerts struct {
-	GrowthNum float64 `json:"growthNum"`
-	Date      time.Time
-	Alerts    []Alert
-	Mail      []string  `json:"mail"`
-	Stmp      mail.Smtp `json:"smtp"`
+	CdnGrowthWarnNum  float64 `json:"cdnGrowthWarnNum"`
+	LiveGrowthWarnNum float64 `json:"liveGrowthWarnNum"`
+	Date              time.Time
+	Alerts            []Alert
+	Mail              []string  `json:"mail"`
+	Stmp              mail.Smtp `json:"smtp"`
 }
 
 // 一条警告
@@ -39,7 +40,7 @@ func (a *Alerts) Calc(m *mysql.Mysql) error {
 	yyesterday := a.Date.AddDate(0, 0, -2).Format("2006-01-02")
 	yesterday := a.Date.AddDate(0, 0, -1).Format("2006-01-02")
 
-	calcfunc := func(a *Alerts, sort, unit, sql string) error {
+	calcfunc := func(a *Alerts, growthWarnNum float64, sort, unit, sql string) error {
 		stmt, err := m.GetStmt(sql)
 		if err != nil {
 			return err
@@ -47,7 +48,7 @@ func (a *Alerts) Calc(m *mysql.Mysql) error {
 		rows, err := stmt.Query(
 			yyesterday,
 			yesterday,
-			a.GrowthNum,
+			growthWarnNum,
 		)
 		if err != nil {
 			return err
@@ -73,6 +74,7 @@ func (a *Alerts) Calc(m *mysql.Mysql) error {
 	// 七牛直播
 	err := calcfunc(
 		a,
+		a.LiveGrowthWarnNum,
 		"七牛直播",
 		"TB",
 		"SELECT tmp.hub,tmp.n1,tmp.n2,ROUND(tmp.n2/tmp.n1,2) AS growth FROM ( SELECT q1.hub, ROUND(JSON_EXTRACT(q1.updown,'$.bytesum')/POWER(1000,4),2) AS n1, ROUND(JSON_EXTRACT(q2.updown,'$.bytesum')/POWER(1000,4),2) AS n2 FROM QiniuHubsFlow q1,QiniuHubsFlow q2 WHERE q1.hub=q2.hub AND q1.date=? AND q2.date=? ) tmp HAVING tmp.n2>0.1 AND growth >?",
@@ -84,6 +86,7 @@ func (a *Alerts) Calc(m *mysql.Mysql) error {
 	// 七牛 cdn
 	err = calcfunc(
 		a,
+		a.CdnGrowthWarnNum,
 		"七牛 cdn",
 		"GB",
 		"SELECT tmp.domain,tmp.n1,tmp.n2,ROUND(tmp.n2/tmp.n1,2) AS growth FROM ( SELECT q1.domain, ROUND(q1.bytesum/POWER(1024,3),2) AS n1, ROUND(q2.bytesum/POWER(1024,3),2) AS n2 FROM QiniuCdnsFlow q1,QiniuCdnsFlow q2 WHERE q1.domain=q2.domain AND q1.date=? AND q2.date=? ) tmp HAVING tmp.n2 >1 AND growth >?",
@@ -95,6 +98,7 @@ func (a *Alerts) Calc(m *mysql.Mysql) error {
 	// 网宿直播
 	err = calcfunc(
 		a,
+		a.LiveGrowthWarnNum,
 		"网宿直播",
 		"TB",
 		"SELECT tmp.channel,tmp.n1,tmp.n2,ROUND(tmp.n2/tmp.n1,2) AS growth FROM ( SELECT w1.channel, ROUND(w1.totalFlow/POWER(1000,2),2) AS n1, ROUND(w2.totalFlow/POWER(1000,2),2) AS n2 FROM WangsuLiveFlow w1,WangsuLiveFlow w2 WHERE w1.channel=w2.channel AND w1.date=? AND w2.date=? ) tmp HAVING tmp.n2>0.1 AND growth >?",
@@ -106,6 +110,7 @@ func (a *Alerts) Calc(m *mysql.Mysql) error {
 	// 网宿 cdn
 	err = calcfunc(
 		a,
+		a.CdnGrowthWarnNum,
 		"网宿 cdn",
 		"GB",
 		"SELECT w1.channel,w1.totalFlow,w2.totalFlow, ROUND(w2.totalFlow/w1.totalFlow,2) AS growth FROM WangsuCdnFlow w1,WangsuCdnFlow w2 WHERE w1.channel=w2.channel AND w1.date=? AND w2.date=? AND w2.totalFlow>1 HAVING growth >?",
@@ -119,7 +124,7 @@ func (a *Alerts) Calc(m *mysql.Mysql) error {
 // 发送邮件
 func (a *Alerts) SendMail() {
 	// 聚合信息
-	subject := fmt.Sprintf("七牛网宿直播 cdn 流量日增幅超过 %d%% 告警！", int(a.GrowthNum*100))
+	subject := "七牛网宿直播 cdn 流量日增幅告警！"
 	content := ""
 
 	yyesterday := a.Date.AddDate(0, 0, -2).Format("2006-01-02")
